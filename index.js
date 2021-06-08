2336d2c8f989536b981cbbd95b965c1463263733
Greg Martin <gregm31@live.com>
2021-06-08 19:42:04 +1000

    Collecting buttons

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 console.log("hello world");
 
-let CURRENT_NUMBER = 0;
+let INITIAL_NUMBER = 0;
+
+let currentCalculation = INITIAL_NUMBER;
 
 // Grab the input screen element
+const intButtons = document.querySelectorAll(".int");
+intButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    console.log(btn.innerText);
+  });
+});
+
 // set the value of input to a currentNumber var
 // add
 //
